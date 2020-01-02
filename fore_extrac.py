ec9deabcf5ef2eb1d60a9d66aadc5f5bce66fea3
Bhanu Gupta <bhanu.tech.contact@gmail.com>
2020-01-02 23:49:03 +0530

    Thresholding and dilation added

--- fore_extrac.py
+++ fore_extrac.py
@@ -7,13 +7,16 @@ bgs = cv2.createBackgroundSubtractorMOG2()
 
 while(1): 
 	ret, frame = cap.read() 
-
 	fmask = bgs.apply(frame) 
 
-	cv2.imshow('Input', frame) 
-	cv2.imshow('Foreground', fmask) 
+	ret, threshold = cv2.threshold(fmask, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU) 
+    
+	kernel = np.ones((5,5),np.uint8)
+	e = cv2.dilate(fmask,kernel,iterations = 1)
 
-	
+	cv2.imshow('Input', frame) 
+	cv2.imshow('Foreground', fmask)	
+
 	k = cv2.waitKey(30) & 0xff
 	if k == 27: 
 		break
