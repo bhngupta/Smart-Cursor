538499fed7d7794ef456903a5b2544bf0daa64a8
Bhanu Gupta <bhanu.tech.contact@gmail.com>
2020-01-10 23:28:00 +0530

    Added deleay for BGS

--- fore_extrac.py
+++ fore_extrac.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2 
 
 cap = cv2.VideoCapture(0) 
-bgs = cv2.createBackgroundSubtractorMOG2() 
+bgs = cv2.createBackgroundSubtractorMOG2(history = 500, varThreshold = 20, detectShadows = True) 
 
 while(1): 
     ret, frame = cap.read() 
@@ -18,13 +18,10 @@ while(1):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     edged = cv2.Canny(gray, 30, 200)
     
-    contours, hierarchy = cv2.findContours(edged,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
 
     cv2.imshow('Input', frame) 
-    #cv2.imshow('Foreground', fmask)
     cv2.imshow('Edges', edged)
-    
+    cv2.imshow('Foreground', fmask)
 
     k = cv2.waitKey(30) & 0xff
     if k == 27: 
